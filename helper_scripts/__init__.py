8b2769a4270e9fb6ac6f9fc79f24f3284fb117ca
Ali Ghorashi <aligho@mit.edu>
2021-03-30 18:58:16 -0400

    imported functions for ease of use

--- helper_scripts/__init__.py
+++ helper_scripts/__init__.py
@@ -1,1 +1,5 @@
-
+#import oscillator_strength
+from . import oscillator_strengths
+from .oscillator_strengths import oscillator_strength
+#from .oscillator_strengths import oscillator_strength_s
+from .oscillator_strengths import oscillator_strength_convergence
--- helper_scripts/oscillator_strengths.py
+++ helper_scripts/oscillator_strengths.py
@@ -7,3 +7,17 @@ def oscillator_strength(filename):
 	ax.set_xlabel('Transition Energy (eV)')
 	ax.set_ylabel('Oscillator Strength')
 	plt.show()
+	print("Check oscillator strength sum rule: ", np.sum(2/3*data[:, 3]*data[:, 6]))
+
+
+def oscillator_strength_convergence(filenames):
+	strengths = np.array([])
+	for file in filenames:
+		data = np.loadtxt(file, skiprows=6)
+		print(np.sum(2/3*data[:, 3]*data[:, 6]))
+		strengths = np.append(strengths, np.sum(2/3*data[:, 3]*data[:, 6]))
+	print(strengths)
+	fig, ax = plt.subplots()
+	ax.plot(strengths)
+	#ax.plot(data[:, 3]*27.2, 2/3*data[:, 3]*data[:, 6])
+	plt.show()
